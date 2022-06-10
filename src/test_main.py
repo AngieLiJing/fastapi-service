d2201d7354e76e842e3b4f06bc06176618122fa2
Angie Li <ali@seismic.com>
2022-06-10 16:03:11 +0800

    change to use request

--- src/test_main.py
+++ src/test_main.py
@@ -1,3 +1,4 @@
+import requests
 from fastapi.testclient import TestClient
 
 from .main import app
@@ -10,9 +11,14 @@ class TestMain:
             print('dddd')
             # client.close()
             exit(0)
+
         except SystemExit:
             pass
-    def test_hello(self):
 
+    def test_hello(self):
+        # client.get('/hello')
+        res = client.request(method='get', url='/hello')
+        print('res=',res.status_code, 'result=', res.json())
+        # requests.get('http://localhost:8000/hello')
         print('hello')
         assert 1==1
