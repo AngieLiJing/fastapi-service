73c1747b730acaf53301765924de8a74d2dd2d89
Angie Li <ali@seismic.com>
2022-06-10 11:15:22 +0800

    simple

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-
 import nest_asyncio
-nest_asyncio.apply()
 
+nest_asyncio.apply()
 app = FastAPI()
+
 
 @app.get('/hello')
 def hello():
-    return {'msg':'hello'}
+    return {'msg': 'hello'}
--- src/test/test_api.py
+++
@@ -1,7 +0,0 @@
-from fastapi.testclient import TestClient
-from src.main import app
-
-client = TestClient(app)
-def test_hello():
-   res = client.get('/hello')
-   assert res.status_code == 200
