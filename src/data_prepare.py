6acf99f4f73cd0c24c51372a9728439faa9c62e9
Angie Li <ali@seismic.com>
2024-07-17 08:44:45 +0800

    copy a to b

--- src/data_prepare.py
+++ src/data_prepare.py
@@ -1,2 +1,8 @@
+with open('src/a.txt', 'r') as read_a:
+    content = read_a.read()
+
+with open('src/b.txt', 'w') as write_b:
+    write_b.write(content)
+
 with open('src/a.txt', 'w') as f:
     pass
