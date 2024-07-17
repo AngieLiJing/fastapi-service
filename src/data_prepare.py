68a6f71981b4f15ec7cdf01cfd7b7c7a49bbc129
Angie Li <ali@seismic.com>
2024-07-17 08:53:44 +0800

    has content in a

--- src/data_prepare.py
+++ src/data_prepare.py
@@ -1,8 +1,10 @@
 with open('src/a.txt', 'r') as read_a:
     content = read_a.read()
+    print(content)
 
-with open('src/b.txt', 'w') as write_b:
-    write_b.write(content)
+if content:
+    with open('src/b.txt', 'w') as write_b:
+        write_b.write(content)
 
-with open('src/a.txt', 'w') as f:
-    pass
+    with open('src/a.txt', 'w') as f:
+        pass
